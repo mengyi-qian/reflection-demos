510c03f239a2ed65c98d4548192d076f8834af6c
Mengyi Qian <mengyi.qian.95@gmail.com>
2023-05-18 18:27:40 +0800

    update

--- js/js-functions.js
+++ js/js-functions.js
@@ -1,10 +1,3 @@
-function displayMessage() {
-    alert('You just clicked a button');
-}
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
 // change shapes
 let square1 = document.getElementById("square1");
 let square2 = document.getElementById("square2");
@@ -16,19 +9,40 @@ function changeShapes() {
 	square3.style.border='10px solid pink';
 }
 
-// change list styles 
+// for ( let i = 0; i < 10; i++ ) {
+// 	console.log( i );
+// }
+
+const grades = [ 90, 50, 75, 100, 85 ];
+let total = 0;
+for ( let i = 0; i < grades.length; i++ ) {
+  total += grades[i];
+  console.log(total);
+}
+console.log( total / grades.length );
+
+
+// change list style
 let items = document.getElementsByTagName('li');
 let yellowItems = document.getElementsByClassName('yellow');
 
 function changeListStyles() {
 
 	for ( let item of items ) {
-	    item.textContent = item.textContent.toUpperCase();
-	    item.style.fontStyle = "italic";
-	}
-	
-	for ( let i = 0; i < yellowItems.length; i++ ) {
-	    yellowItems[i].style.backgroundColor = 'gold';
+		item.textContent = item.textContent.toLocaleUpperCase();
+		item.style.fontStyle = "italic";
 	}
 
+	for ( let i = 0; i < yellowItems.length; i++ ) {
+		yellowItems[i].style.background = "gold";
+	}
+
+}
+
+
+function displayMessage() {
+    alert('You just clicked a button');
+}
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
